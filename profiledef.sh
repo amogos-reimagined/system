0cfa2deeec9d4bc279c49618dbd176113501dfb0
Damian Leguła <damian.legula@proton.me>
2026-07-26 16:32:21 +0200

    it works
    in grub press tab and typae copytoram=0

--- profiledef.sh
+++ profiledef.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="AmogOS-reimagined"
+iso_label="AmogOS_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="AmogOS Linux <https://github.com/amogos-reimagined>"
+iso_application="amogos-reimagined"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
   'uefi.systemd-boot')
+DEFAULT_KERNEL_PARAMS="copytoram=0"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
@@ -27,4 +28,5 @@ services=(
   "systemd-networkd"
   "systemd-resolved"
   "gdm"
+  "calamares-autostart"
 )
